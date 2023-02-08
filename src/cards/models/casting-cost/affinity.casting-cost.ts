e976050132fd19788e44b78d10494651aa35d952
Eric <eric-417@live.com>
2023-02-07 23:18:30 -0500

    Ref bug with afiinity

--- src/cards/models/casting-cost/affinity.casting-cost.ts
+++ src/cards/models/casting-cost/affinity.casting-cost.ts
@@ -3,6 +3,7 @@ import { Card, ManaType } from '~/models/card.model';
 import { ZoneType } from '~/models/zone.model';
 import { castSpell } from '~/states';
 import { CastingCost } from './casting-cost';
+import { clone } from 'ramda';
 
 export class AffinityCastingCost extends CastingCost {
   constructor(card: Card, label: string, private cardType = 'Artifact', validZones = [ZoneType.hand]) {
@@ -28,10 +29,11 @@ export class AffinityCastingCost extends CastingCost {
       return;
     }
 
-    const found = this.getMeta().baseCard.manaCost.mana.find((mana) => mana.genericCost > 0) || { genericCost: 0, types: [] };
+    const cloned = clone(this.getMeta());
+    const found = cloned.baseCard.manaCost.mana.find((mana) => mana.genericCost > 0) || { genericCost: 0, types: [] };
 
     found.genericCost -= numberOfArtifacts;
 
-    castSpell(this.getMeta().baseCard, { cardPos: this.getMeta().position, skipAuto: true });
+    castSpell(cloned.baseCard, { cardPos: this.getMeta().position, skipAuto: true });
   }
 }
--- src/subjects/zone-changes.subject.ts
+++ src/subjects/zone-changes.subject.ts
@@ -1,7 +1,15 @@
 import { Subject } from 'rxjs';
+import { useGameState } from '~/composables';
 import { ZoneType } from '~/models/zone.model';
-import { Card } from '~/models/card.model';
 
 const zoneChangesSubject = new Subject<{ newZone: ZoneType; cardIds: string[] }>();
+
+zoneChangesSubject.subscribe(({ newZone, cardIds }) => {
+  const { setMeta } = useGameState();
+  if (newZone !== ZoneType.battlefield) {
+    // clear states and counters
+    cardIds.forEach((id) => setMeta(id, { isTapped: false }));
+  }
+});
 
 export { zoneChangesSubject };
